a5cda660848a508d3707ac60497037bae7bd1169
KrystianSA <ksasiadek64@gmail.com>
2023-01-23 18:58:03 +0100

    dzien 6

--- ChallengeApp/ChallengeApp/Employee.cs
+++ ChallengeApp/ChallengeApp/Employee.cs
@@ -4,71 +4,23 @@ namespace ChallengeApp
 {
     class Employee
     {
-        private string name;
-        private string surname;
-        private string age;
-        private List<int> Grades1 = new List<int>();
-        private List<int> Grades2 = new List<int>();
-        private List<int> Grades3 = new List<int>();
-        private List<string> personalData = new List<string>();
+ 
+        private List<int> Grades = new List<int>();
 
         public Employee(string name, string surename, string age)
         {
-            this.name = name;
-            this.surname = surename;
-            this.age = age;
+            this.Name = name;
+            this.Surname = surename;
+            this.Age = age;
         }
-
-        public void AddGrades1(int number1, int number2, int number3, int number4, int number5)
+        public string Name { get; private set; }
+        public string Surname { get; private set; }
+        public string Age { get; private set; }
+        public void AddGrades(int number)
         {
-            this.Grades1.Add(number1);
-            this.Grades1.Add(number2);
-            this.Grades1.Add(number3);
-            this.Grades1.Add(number4);
-            this.Grades1.Add(number5);
+            this.Grades.Add(number);
         }
 
-        public void AddGrades2(int number1, int number2, int number3, int number4, int number5)
-        {
-            this.Grades1.Add(number1);
-            this.Grades1.Add(number2);
-            this.Grades1.Add(number3);
-            this.Grades1.Add(number4);
-            this.Grades1.Add(number5);
-        }
-
-        public void AddGrades3(int number1, int number2, int number3, int number4, int number5)
-        {
-            this.Grades1.Add(number1);
-            this.Grades1.Add(number2);
-            this.Grades1.Add(number3);
-            this.Grades1.Add(number4);
-            this.Grades1.Add(number5);
-        }
-
-        public int SumGrades { get { return this.Grades1.Sum(); } }
-        
-        public string Showname
-        { 
-            get 
-            {
-                return this.name;
-            } 
-        }
-        public string Showsurname
-        { 
-            get
-            {
-                return this.surname;
-            }
-        }
-        public string ShowAge
-        {
-            get
-            {
-                return this.age;
-            }
-        }
-
+        public int SumGrades { get { return this.Grades.Sum(); } }
     }
 }
