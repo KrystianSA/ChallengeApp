a022f6e3dd8c1d14fa4d3e51f2a7af3f7b23de1d
KrystianSA <ksasiadek64@gmail.com>
2023-02-02 19:53:40 +0100

    zadanie, dzień 8

---
+++ ChallengeApp/ChallengeApp.tests1/EmployeeTests.cs
@@ -0,0 +1,49 @@
+﻿using System.Drawing;
+
+namespace ChallengeApp.tests1
+{
+    public class EmployeeTests
+    {
+        [Test]
+        public void WhenNumberIsTheHighest_GetCorrectResult()
+        {
+            var employee = new Employee("Krystian", "Sąsiadek");
+            employee.AddGrade(1);
+            employee.AddGrade(3);
+            employee.AddGrade(6);
+
+            var result = employee.GetStatistics();
+            var statistic = result.Max;
+
+            Assert.AreEqual(6,statistic);
+        }
+
+        [Test]
+        public void WhenNumberIsTheLowest_GetCorrectResult()
+        {
+            var employee = new Employee("Krystian", "Sąsiadek");
+            employee.AddGrade(1);
+            employee.AddGrade(3);
+            employee.AddGrade(6);
+
+            var result = employee.GetStatistics();
+            var statistic = result.Min;
+
+            Assert.AreEqual(1,statistic);
+        }
+        [Test]
+        public void WhenNumberAvarageIsCorrect_GetCorrectResult()
+        {
+            var employee = new Employee("Krystian", "Sąsiadek");
+            employee.AddGrade(1);
+            employee.AddGrade(3);
+            employee.AddGrade(6);
+
+            var result = employee.GetStatistics();
+            var statistic = result.Avarage;
+
+            Assert.AreEqual(4,statistic);
+      
+        }
+    }
+}
