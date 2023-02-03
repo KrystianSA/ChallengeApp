cc40864eeb2fd6f5e05ca61ed024e5bd8be77195
KrystianSA <ksasiadek64@gmail.com>
2023-02-03 19:34:39 +0100

    zadanie dzień 11

--- ChallengeApp/ChallengeApp/Employee.cs
+++ ChallengeApp/ChallengeApp/Employee.cs
@@ -1,26 +1,133 @@
 ﻿using ChallengeApp;
+using System;
+using System.Diagnostics;
 
 namespace ChallengeApp
 {
-    class Employee
+    public class Employee
     {
- 
-        private List<int> Grades = new List<int>();
 
-        public Employee(string name, string surename, string age)
+        private List<float> grades = new List<float>();
+
+        public Employee(string name, string surname)
         {
             this.Name = name;
-            this.Surname = surename;
-            this.Age = age;
-        }
-        public string Name { get; private set; }
-        public string Surname { get; private set; }
-        public string Age { get; private set; }
-        public void AddGrades(int number)
-        {
-            this.Grades.Add(number);
+            this.Surname = surname;
         }
 
-        public int SumGrades { get { return this.Grades.Sum(); } }
+        public string Name { get; private set; }
+        public string Surname { get; private set; }
+
+        public void AddGrade(float grade)
+        {
+            if (grade >= 0 && grade <= 100) { this.grades.Add(grade); }
+            else { Console.WriteLine("invalid grade value"); }
+        }
+
+        public void AddGrade(double grade)
+        {
+            float valueInFloat = (float)grade;
+            if (grade >= 0 && grade <= 100) { this.grades.Add(valueInFloat); }
+            else { Console.WriteLine("invalid grade value"); }
+        }
+
+        public void AddGrade(string grade)
+        {
+            if (float.TryParse(grade, out float result))
+            {
+                this.AddGrade(result);
+            }
+            else { Console.WriteLine("string is not float"); }
+        }
+
+        public void AddGrade(char grade)
+        {
+            if (float.TryParse(grade.ToString(), out float result))
+            {
+                this.AddGrade(result);
+            }
+            else { Console.WriteLine("char is not float"); }
+
+        }
+
+        public Statistics GetStatistics()
+        {
+            var statistics = new Statistics();
+            statistics.Avarage = 0;
+            statistics.Max = float.MinValue;
+            statistics.Min = float.MaxValue;
+
+            foreach (var grade in this.grades)
+            {
+                statistics.Max = Math.Max(statistics.Max, grade);
+                statistics.Min = Math.Min(statistics.Min, grade);
+                statistics.Avarage += grade;
+            }
+
+            statistics.Avarage = statistics.Avarage / this.grades.Count;
+
+            return statistics;
+        }
+
+        public Statistics GetStatisticsWithFor()
+        {
+            var statistics = new Statistics();
+            statistics.Avarage = 0;
+            statistics.Max = float.MinValue;
+            statistics.Min = float.MaxValue;
+
+            for (int index = 0; index < this.grades.Count; index++)
+            {
+                statistics.Max = Math.Max(statistics.Max, grades[index]);
+                statistics.Min = Math.Min(statistics.Min, grades[index]);
+                statistics.Avarage += grades[index];
+            }
+            statistics.Avarage = statistics.Avarage / this.grades.Count;
+
+            return statistics;
+        }
+
+        public Statistics GetStatisticsWithDoWhile()
+        {
+            var statistics = new Statistics();
+            statistics.Avarage = 0;
+            statistics.Max = float.MinValue;
+            statistics.Min = float.MaxValue;
+
+            var index = 0;
+
+            do
+            {
+                statistics.Max = Math.Max(statistics.Max, grades[index]);
+                statistics.Min = Math.Min(statistics.Min, grades[index]);
+                statistics.Avarage += grades[index];
+                index++;
+            } while (index < this.grades.Count);
+
+            statistics.Avarage = statistics.Avarage / this.grades.Count;
+
+            return statistics;
+        }
+        public Statistics GetStatisticsWithWhile()
+        {
+            var statistics = new Statistics();
+            statistics.Avarage = 0;
+            statistics.Max = float.MinValue;
+            statistics.Min = float.MaxValue;
+
+            var index = 0;
+
+            while (index < this.grades.Count)
+            {
+                statistics.Max = Math.Max(statistics.Max, grades[index]);
+                statistics.Min = Math.Min(statistics.Min, grades[index]);
+                statistics.Avarage += grades[index];
+                index++;
+            }
+
+            statistics.Avarage = statistics.Avarage / this.grades.Count;
+
+            return statistics;
+        }
     }
 }
--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,74 +1,34 @@
-﻿int count0 = 0, count1 = 0, count2 = 0, count3 = 0, count4 = 0, count5 = 0, count6 = 0, count7 = 0, count8 = 0, count9 = 0;
-int number = 4566;
-string numberToString = number.ToString();
-char[] letters = numberToString.ToCharArray();
-char[] letters2 = { '1', '2', '3', '4', '5', '6', '7', '8', '9' };
+﻿using ChallengeApp;
 
-for (int i = 0; i < letters.Length; i++)
-{
-    for (int j = 0; j < letters2.Length; j++)
-    {
-        if (letters[i] == '0' && letters2[j] == '0')
-        {
-            count0++;
-        }
-        if (letters[i] == '1' && letters2[j] == '1')
-        {
-            count1++;
-        }
-        if (letters[i] == '2' && letters2[j] == '2')
-        {
-            count2++;
-        }
-        if (letters[i] == '3' && letters2[j] == '3')
-        {
-            count3++;
-        }
-        if (letters[i] == '4' && letters2[j] == '4')
-        {
-            count4++;
-        }
-        if (letters[i] == '5' && letters2[j] == '5')
-        {
-            count5++;
-        }
-        if (letters[i] == '6' && letters2[j] == '6')
-        {
-            count6++;
-        }
-        if (letters[i] == '7' && letters2[j] == '7')
-        {
-            count7++;
-        }
-        if (letters[i] == '8' && letters2[j] == '8')
-        {
-            count8++;
-        }
-        if (letters[i] == '9' && letters2[j] == '9')
-        {
-            count9++;
-        }
-    }
-}
+var employee = new Employee("Krystian", "Sąsiadek");
+employee.AddGrade("1");
+employee.AddGrade(1);
+employee.AddGrade(1);
+employee.AddGrade(1);
+var statistics1 = employee.GetStatistics();
+var statistics2 = employee.GetStatisticsWithFor();
+var statistics3 = employee.GetStatisticsWithDoWhile();
+var statistics4 = employee.GetStatisticsWithWhile();
 
-Console.WriteLine("Wyniki dla liczby 4566 ");
-Console.Write("0 => ");
-Console.WriteLine(count0);
-Console.Write("1 => ");
-Console.WriteLine(count1);
-Console.Write("2 => ");
-Console.WriteLine(count2);
-Console.Write("3 => ");
-Console.WriteLine(count3);
-Console.Write("4 => ");
-Console.WriteLine(count4);
-Console.Write("5 => ");
-Console.WriteLine(count5);
-Console.Write("6 => ");
-Console.WriteLine(count6);
-Console.Write("7 => ");
-Console.WriteLine(count7);
-Console.Write("8 => ");
-Console.WriteLine(count8);
-Console.Write("9 => ");
-Console.WriteLine(count9);
+Console.WriteLine("Pętla foreach");
+Console.WriteLine($"Avarage: {statistics1.Avarage:N2} ");
+Console.WriteLine($"Min: {statistics1.Min} ");
+Console.WriteLine($"Max: {statistics1.Max} ");
+Console.WriteLine(" ");
+
+Console.WriteLine("Pętla for");
+Console.WriteLine($"Avarage: {statistics2.Avarage:N2} ");
+Console.WriteLine($"Min: {statistics2.Min} ");
+Console.WriteLine($"Max: {statistics2.Max} ");
+Console.WriteLine(" ");
+
+Console.WriteLine("Pętla do while");
+Console.WriteLine($"Avarage: {statistics3.Avarage:N2} ");
+Console.WriteLine($"Min: {statistics3.Min} ");
+Console.WriteLine($"Max: {statistics3.Max} ");
+Console.WriteLine(" ");
+
+Console.WriteLine("Pętla while");
+Console.WriteLine($"Avarage: {statistics4.Avarage:N2} ");
+Console.WriteLine($"Min: {statistics4.Min} ");
+Console.WriteLine($"Max: {statistics4.Max} ");
